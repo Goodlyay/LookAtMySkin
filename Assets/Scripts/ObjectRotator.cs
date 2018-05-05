6685be6c06bd9da5c5d32835fb8a5cf94ebee016
Goodlyay <zackxv7@yahoo.com>
2018-05-05 04:04:06 -0700

    Project dump

---
+++ Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotator : MonoBehaviour 
+{
+	
+	public float _sensitivity;
+	private Vector3 _mouseReference;
+	private Vector3 _mouseOffset;
+	private Vector3 _rotation;
+	private bool _isRotating;
+
+	private static bool partRotating;
+	public bool rotateParent;
+
+	public float rotLimitMaxX;
+	public float rotLimitMinX;
+
+	public float rotLimitMaxY;
+	public float rotLimitMinY;
+
+	private Vector3 originalRot;
+	private int doAgain;
+
+
+	
+	void Start ()
+	{
+		_sensitivity = 0.4f;
+		_rotation = Vector3.zero;
+		if(rotateParent)
+			originalRot = transform.parent.eulerAngles;
+		else
+			originalRot = transform.eulerAngles;
+	}
+	
+	void Update()
+	{
+
+		if (Input.GetKeyDown (KeyCode.Backspace)) {
+			if (!partRotating || _isRotating)
+				doAgain = 2;
+		}
+		if (doAgain > 0){
+			if(!rotateParent)
+				transform.eulerAngles = originalRot;
+			else
+				transform.parent.eulerAngles = originalRot;
+			--doAgain;
+		}
+
+
+		if (rotateParent && !partRotating) {
+			if (Input.GetMouseButtonDown(0))
+				OnMouseDown ();
+
+			if (Input.GetMouseButtonUp(0))
+				OnMouseUp ();
+
+		}
+
+		if(_isRotating)
+		{
+			// offset
+			_mouseOffset = (Input.mousePosition - _mouseReference);
+			
+			// apply rotation
+			_rotation.x = -(_mouseOffset.x) * _sensitivity;
+			_rotation.y = -(_mouseOffset.y) * _sensitivity;
+			
+			// rotate
+			if (rotateParent){
+
+				//transform.parent.Rotate(_rotation);
+				transform.parent.RotateAround(transform.parent.position,transform.parent.up,_rotation.x);
+				Camera.main.transform.RotateAround(transform.parent.position,Camera.main.transform.right,_rotation.y);
+			}
+			else {
+				if(gameObject.name == "Head"){
+					transform.RotateAround(transform.position,transform.up,-_rotation.y); // This is the one that nods yes
+					transform.RotateAround(transform.position,transform.parent.up,_rotation.x); //This is the one that shakes no
+				}
+				else{
+					//transform.Rotate(_rotation);
+					transform.RotateAround(transform.position,Camera.main.transform.forward,-_rotation.x);
+					transform.RotateAround(transform.position,Camera.main.transform.right,-_rotation.y);
+				}
+			}
+			
+			// store mouse
+			_mouseReference = Input.mousePosition;
+		}
+	}
+	
+	void OnMouseDown()
+	{
+		// rotating flag
+		_isRotating = true;
+		if (!rotateParent)
+			partRotating = true;
+		
+		// store mouse
+		_mouseReference = Input.mousePosition;
+	}
+	
+	void OnMouseUp()
+	{
+		if (!rotateParent)
+			partRotating = false;
+		// rotating flag
+		_isRotating = false;
+	}
+	
+}
---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+	public GameObject head;
+	public GameObject body;
+	public GameObject rightArm;
+	public GameObject leftArm;
+	public GameObject rightLeg;
+	public GameObject leftLeg;
+
+	private Texture2D customTexture;
+
+	private static float originalColorValueRGB = 0.5f;
+	private static float newColorValueRGB = 0.737f;
+
+	private Color originalColor = new Color(originalColorValueRGB, originalColorValueRGB, originalColorValueRGB, 1.0F);
+
+	private Vector3 cameraStartLocation;
+	private Vector3 cameraStartRotation;
+
+	private int currentModelShown = 0; //0 is 1.8 1 is 1.7 3 is alex
+	public int myModelNumber;
+
+	//private Vector3 theForwardDirection = Camera.main.transform.TransformDirection (Vector3.forward);
+
+	public string url = "file://edit.png";//"https://dl.dropboxusercontent.com/u/12694594/skins/Chief.png";
+	// Use this for initialization
+	void Start () {
+		UpdateTextures ();
+		newColorValueRGB = originalColorValueRGB;
+		cameraStartLocation = Camera.main.transform.position;
+		cameraStartRotation = Camera.main.transform.eulerAngles;
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.M)) {
+			currentModelShown++;
+			if (currentModelShown >= 3)
+				currentModelShown = 0;
+			body.transform.parent.gameObject.SetActive (currentModelShown == myModelNumber);
+
+		}
+
+
+
+
+		if (Input.GetKeyDown ("space"))
+			UpdateTextures ();
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			newColorValueRGB = newColorValueRGB - 0.03f;
+			Color newColor = new Color(newColorValueRGB, newColorValueRGB, newColorValueRGB, 1.0F);
+			Camera.main.backgroundColor = newColor;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			newColorValueRGB = originalColorValueRGB;
+			Camera.main.backgroundColor = originalColor;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			newColorValueRGB = newColorValueRGB + 0.03f;
+			Color newColor = new Color(newColorValueRGB, newColorValueRGB, newColorValueRGB, 1.0F);
+			Camera.main.backgroundColor = newColor;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			newColorValueRGB = newColorValueRGB + 0.03f;
+			Color newColor = new Color(0.3058f, 0.7176f, 0.6352f, 1.0F);
+			Camera.main.backgroundColor = newColor;
+		}
+
+
+
+        if (Input.GetKeyDown (KeyCode.F2))
+        {
+            DateTime date = new DateTime();
+            date = DateTime.UtcNow;
+            Application.CaptureScreenshot("..\\Screenshot" + date.ToFileTimeUtc() + ".png");
+        }
+
+
+        //Change Fov
+        /*
+		if (Input.GetKey (KeyCode.Alpha4)) {
+			Camera.main.fieldOfView = Camera.main.fieldOfView + 2;
+			Camera.main.transform.Translate(Camera.main.transform.forward * 0.05f,Space.World);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha5)) {
+			Camera.main.fieldOfView = originalFov;
+		}
+		if (Input.GetKey (KeyCode.Alpha6)) {
+			Camera.main.fieldOfView = Camera.main.fieldOfView - 2;
+			Camera.main.transform.Translate(Camera.main.transform.forward * -0.05f,Space.World);
+		}
+		*/
+
+        if (Input.GetMouseButtonDown (2)) {
+			Camera.main.transform.position = cameraStartLocation;
+			Camera.main.transform.eulerAngles = cameraStartRotation;
+		}
+
+		if(Input.GetAxis("Mouse ScrollWheel") > 0)
+			Camera.main.transform.Translate(Camera.main.transform.forward * 0.35f,Space.World);
+		if(Input.GetAxis("Mouse ScrollWheel") < 0)
+			Camera.main.transform.Translate(Camera.main.transform.forward * -0.35f,Space.World);
+
+
+	}
+
+	IEnumerator LoadTexture(GameObject bodyPart) {
+
+		Renderer renderer = bodyPart.GetComponent<Renderer> ();
+		
+		// Create a texture in DXT1 format
+		//renderer.material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+		
+		// Start a download of the given URL
+		WWW www = new WWW(url);
+		
+		// wait until the download is done
+		yield return www;
+		
+		// assign the downloaded image to the main texture of the object
+		renderer.sharedMaterial.mainTexture = www.texture; //www.LoadImageIntoTexture(renderer.material.mainTexture);
+		renderer.sharedMaterial.mainTexture.filterMode = FilterMode.Point;
+	}
+
+	void UpdateTextures(){
+		StartCoroutine (LoadTexture (head));
+		StartCoroutine (LoadTexture (body));
+		StartCoroutine (LoadTexture (rightArm));
+		StartCoroutine (LoadTexture (leftArm));
+		StartCoroutine (LoadTexture (rightLeg));
+		StartCoroutine (LoadTexture (leftLeg));
+	}
+
+}
